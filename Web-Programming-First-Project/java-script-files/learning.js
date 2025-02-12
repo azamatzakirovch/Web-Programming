fbfa6ccb6a551179f583768872b3c3a0718fc4cf
Azamat Zakirovch <a.eshqoziyev@newuu.uz>
2025-02-13 00:14:00 +0500

    done ! I write the names of all courses My bro

--- Web-Programming-First-Project/java-script-files/learning.js
+++ Web-Programming-First-Project/java-script-files/learning.js
@@ -11,6 +11,20 @@ img_of_button_for_AI.alt = "AI";
 button_for_AI.appendChild(img_of_button_for_AI);
 root.appendChild(button_for_AI);
 
+const ai = document.createElement("div");
+ai.textContent = "Artificial Intelligence";
+ai.style.fontSize = "30px";
+ai.style.fontFamily = "Arial";
+ai.style.position = "absolute";
+ai.style.left = "260px";
+ai.style.top = "540px";
+ai.style.color = "#000000";
+
+root.appendChild(ai);
+
+
+
+
 const button_for_Data_Science = document.createElement("div");
 button_for_Data_Science.className = "button_for_Data_Science";
 
@@ -21,6 +35,19 @@ img_of_button_for_Data_Science.alt = "Data Science";
 
 button_for_Data_Science.appendChild(img_of_button_for_Data_Science);
 root.appendChild(button_for_Data_Science);
+
+const data = document.createElement("div");
+data.textContent = "Data Science";
+data.style.fontSize = "30px";
+data.style.fontFamily = "Arial";
+data.style.position = "absolute";
+data.style.left = "700px";
+data.style.top = "540px";
+data.style.color = "#000000";
+root.appendChild(data);
+
+
+
 
 const button_for_ML = document.createElement("div");
 button_for_ML.className = "button_for_ML";
@@ -33,6 +60,21 @@ img_of_button_for_ML.alt = "Machine Learning";
 button_for_ML.appendChild(img_of_button_for_ML);
 root.appendChild(button_for_ML);
 
+const ML = document.createElement("div");
+ML.textContent = "Machine Learning";
+ML.style.fontSize = "30px";
+ML.style.fontFamily = "Arial";
+ML.style.position = "absolute";
+ML.style.left = "1090px";
+ML.style.top = "540px";
+ML.style.color = "#000000";
+root.appendChild(ML);
+
+
+
+
+
+
 const button_for_DL = document.createElement("div");
 button_for_DL.className = "button_for_DL";
 
@@ -43,6 +85,23 @@ img_of_button_for_DL.alt = "Deep Learning";
 
 button_for_DL.appendChild(img_of_button_for_DL);
 root.appendChild(button_for_DL);
+
+const DL = document.createElement("div");
+DL.textContent = "Deep Learning";
+DL.style.fontSize = "30px";
+DL.style.fontFamily = "Arial";
+DL.style.position = "absolute";
+DL.style.left = "1530px";
+DL.style.top = "540px";
+DL.style.color = "#000000";
+root.appendChild(DL);
+
+
+// 260 700 1090 1530
+
+
+
+
 
 const button_for_Neural_networks = document.createElement("div");
 button_for_Neural_networks.className = "button_for_Neural_networks";
@@ -55,6 +114,20 @@ img_for_button_for_Neural_networks.alt = "Neural Networks";
 button_for_Neural_networks.appendChild(img_for_button_for_Neural_networks);
 root.appendChild(button_for_Neural_networks);
 
+const NN = document.createElement("div");
+NN.textContent = "Neural Networks";
+NN.style.fontSize = "30px";
+NN.style.fontFamily = "Arial";
+NN.style.position = "absolute";
+NN.style.left = "280px";
+NN.style.top = "950px";
+NN.style.color = "#000000";
+root.appendChild(NN);
+
+
+
+
+
 const button_for_Linear_Algebra = document.createElement("div");
 button_for_Linear_Algebra.className = "button_for_Linear_Algebra";
 
@@ -65,6 +138,19 @@ img_of_button_for_Linear_Algebra.alt = "Linear Algebra";
 
 button_for_Linear_Algebra.appendChild(img_of_button_for_Linear_Algebra);
 root.appendChild(button_for_Linear_Algebra);
+
+const LA = document.createElement("div");
+LA.textContent = "Linear Algebra";
+LA.style.fontSize = "30px";
+LA.style.fontFamily = "Arial";
+LA.style.position = "absolute";
+LA.style.left = "700px";
+LA.style.top = "950px";
+LA.style.color = "#000000";
+root.appendChild(LA);
+
+
+
 
 const button_for_Probability_and_Statistics = document.createElement("div");
 button_for_Probability_and_Statistics.className = "button_for_Probability_and_Statistics";
@@ -77,6 +163,19 @@ img_of_button_for_Probability_and_Statistics.alt = "Probability and Statistics";
 button_for_Probability_and_Statistics.appendChild(img_of_button_for_Probability_and_Statistics);
 root.appendChild(button_for_Probability_and_Statistics);
 
+const PAS = document.createElement("div");
+PAS.textContent = "Probability & Statistics";
+PAS.style.fontSize = "30px";
+PAS.style.fontFamily = "Arial";
+PAS.style.position = "absolute";
+PAS.style.left = "1065px";
+PAS.style.top = "950px";
+PAS.style.color = "#000000";
+root.appendChild(PAS);
+
+
+
+
 const button_for_Calculus = document.createElement("div");
 button_for_Calculus.className = "button_for_Calculus";
 
@@ -87,3 +186,13 @@ img_of_button_for_Calculus.alt = "Calculus";
 
 button_for_Calculus.appendChild(img_of_button_for_Calculus);
 root.appendChild(button_for_Calculus);
+
+const C = document.createElement("div");
+C.textContent = "Calculus";
+C.style.fontSize = "30px";
+C.style.fontFamily = "Arial";
+C.style.position = "absolute";
+C.style.left = "1560px";
+C.style.top = "950px";
+C.style.color = "#000000";
+root.appendChild(C);
